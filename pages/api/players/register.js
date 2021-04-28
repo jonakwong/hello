9f4a63a68987777c677ee1b86c04c157e12b1911
yeuk fei kwong <jonayfkwong@hotmail.com>
2021-04-29 00:00:50 +0800

    deploy with git

--- pages/api/players/register.js
+++ pages/api/players/register.js
@@ -1,25 +1,21 @@
 import { connectToDatabase } from "../../../util/mongodb";
 import { registerValidation } from "../../../models/validation"
-const bcrypt = require('bcrypt')
 
 export default async (request, response) => {
     const { db } = await connectToDatabase();
 
 
-    //checking if the user is aleardy in the database
-    const usernameExist = await db.collection("players").findOne({ username: request.body.username });
-    if (usernameExist) return response.send('username aleardy exists');
 
-        const data = await db
-            .collection("players")
-            .insertOne({
-                fullName: request.body.fullName,
-                username: request.body.username,
-                phonenumber: request.body.phonenumber,
-                password: request.body.password,
-            })
+    const data = await db
+        .collection("players")
+        .insertOne({
+            fullName: request.body.fullName,
+            username: request.body.username,
+            phonenumber: request.body.phonenumber,
+            password: request.body.password,
+        })
 
-        const selectedplayer = await db.collection("players").findOne({ username: request.body.username })
-        response.send({ _id: selectedplayer._id })
+    const selectedplayer = await db.collection("players").findOne({ username: request.body.username })
+    response.send({ _id: selectedplayer._id })
 
 };
