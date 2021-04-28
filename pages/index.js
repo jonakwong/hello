d11790ff8e059cf5d3aee6c5f8ae29ca5aa2243f
yeuk fei kwong <jonayfkwong@hotmail.com>
2021-04-28 15:01:57 +0800

    deploy with git

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1>hello jonathan</h1>
+        <h1>hello jonathan 2</h1>
       </main>
 
       <footer className={styles.footer}>
