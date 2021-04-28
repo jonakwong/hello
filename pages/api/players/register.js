19cb9dc38fe5c381110e8df93c965b6235b7d7eb
yeuk fei kwong <jonayfkwong@hotmail.com>
2021-04-28 16:29:15 +0800

    deploy with git

--- pages/api/players/register.js
+++ pages/api/players/register.js
@@ -7,24 +7,28 @@ export default async (request, response) => {
 
     //Validataion & Date  before we create a player
     const { error } = registerValidation(request.body);
-    if (error) return response.send(error.details[0].message)
+    if (error) return response.send({ "message": error.details[0].message })
 
     //checking if the user is aleardy in the database
     const usernameExist = await db.collection("players").findOne({ username: request.body.username });
     if (usernameExist) return response.send({ 'message': 'username aleardy exists' });
 
+    try {
+        const data = await db
+            .collection("players")
+            .insertOne({
+                fullName: request.body.fullName,
+                username: request.body.username,
+                phonenumber: request.body.phonenumber,
+                password: request.body.password,
+            })
 
-    const data = await db
-        .collection("players")
-        .insertOne({
-            fullName: request.body.fullName,
-            username: request.body.username,
-            phonenumber: request.body.phonenumber,
-            password: request.body.password,
-        })
+        const selectedplayer = await db.collection("players").findOne({ username: request.body.username })
+        response.send({ _id: selectedplayer._id })
+    } catch (error) {
+        response.send({ "error": error })
+    }
 
-    const selectedplayer = await db.collection("players").findOne({ username: request.body.username })
-    response.send({ _id: selectedplayer._id })
 
 
 
