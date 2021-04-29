6371421e4bc114dbd451f38bc45866e11baf40b0
yeuk fei kwong <jonayfkwong@hotmail.com>
2021-04-29 13:30:23 +0800

    deploy with git

--- pages/login.js
+++ pages/login.js
@@ -1,121 +1,124 @@
 
 import { useState, useEffect } from 'react'
-
+const bcrypt = require('bcrypt')
 
 export default function Login() {
-    const Logo = '../Material/Logo.png'
-    const [LogResBoolean, setLogResBoolean] = useState(true) //true= Page Register
-    const [PageTitle, setPageTitle] = useState()
-    const [Login_Register_Toggle, setLogin_Sign_Toggle] = useState()
+  const Logo = '../Material/Logo.png'
+  const [LogResBoolean, setLogResBoolean] = useState(true) //true= Page Register
+  const [PageTitle, setPageTitle] = useState()
+  const [Login_Register_Toggle, setLogin_Sign_Toggle] = useState()
 
-    useEffect(() => {
-        //setpage boolean
-        if (LogResBoolean == true) {
-            setPageTitle('Register')
-            setLogin_Sign_Toggle('Login')
+  useEffect(() => {
+    //setpage boolean
+    if (LogResBoolean == true) {
+      setPageTitle('Register')
+      setLogin_Sign_Toggle('Login')
 
-        } else {
-            setPageTitle('Login')
-            setLogin_Sign_Toggle('Register')
-        }
+    } else {
+      setPageTitle('Login')
+      setLogin_Sign_Toggle('Register')
+    }
+  })
+
+  //define capture data (login details by user)
+  const [details, setDetails] = useState({
+    username: "",
+    password: ""
+  });
+
+  //handle login form submit
+  const submitHandler = e => {
+    e.preventDefault();
+    console.log(details)
+
+    //Hash password
+    const salt = await bcrypt.genSalt(8);
+    const hashedPassword = await bcrypt.hash(details.password, salt)
+    setDetails({ ...details, password: hashedPassword })
+
+    if (LogResBoolean == true) { //if true = register > create account
+      registerAccount(details)
+    } else { //else false = login > login account
+      loginAccount(details)
+    }
+  }
+
+  //register quest to api
+  async function registerAccount(details) {
+    const registerurl = '/api/players/register'
+
+    await fetch(registerurl, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: (JSON.stringify({
+        username: details.username,
+        password: details.password
+      }))
     })
+      .then(response => response.json())
+      .catch(error => console.error('Error:', error))
+      .then(response => console.log('Success:', JSON.stringify(response)))
 
-    //define capture data (login details by user)
-    const [details, setDetails] = useState({
-        username: "",
-        password: ""
-    });
-
-    //handle login form submit
-    const submitHandler = e => {
-        e.preventDefault();
-        console.log(details)
-
-        if (LogResBoolean == true) { //if true = register > create account
-            registerAccount(details)
-        } else { //else false = login > login account
-            loginAccount(details)
-        }
-
-    }
-
-
-    //register quest to api
-    async function registerAccount(details) {
-        const registerurl = '/api/players/register'
-
-        await fetch(registerurl, {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: (JSON.stringify({
-                username: details.username,
-                password: details.password
-            }))
-        })
-            .then(response => response.json())
-            .catch(error => console.error('Error:', error))
-            .then(response => console.log('Success:', JSON.stringify(response)))
-
-    }
-    //login quest to api
+  }
+  //login quest to api
 
 
 
-    return (
-        <div className="LoginSignup_page">
-            <div className={`${!LogResBoolean && "LoginContainer"} ${LogResBoolean && "SignupContainer"}`}>
+  return (
+    <div className="LoginSignup_page">
+      <div className={`${!LogResBoolean && "LoginContainer"} ${LogResBoolean && "SignupContainer"}`}>
 
-                {/* LogoContainer*/}
-                <div className="Logocontainer">
+        {/* LogoContainer*/}
+        <div className="Logocontainer">
 
-                </div>
-                {/* PageTitle*/}
-                <div className="Pagetitlecontainer">
-                    <h3>{PageTitle}</h3>
-                </div>
-
-
-                {/*Form */}
-                <form className="Login_form_container" onSubmit={submitHandler}>
-                    <div className="errorcontainer">
-
-                    </div>
-
-                    {/*Container*/}
-                    <div className="Login_Signup_Input_Container">
-                        <div className={`${LogResBoolean && "Login_Input_container_noshow"}  ${!LogResBoolean && "Login_Input_container "}`}>
-                            <div className="Login_Input_Field">
-                                <input placeholder="Login" onChange={e => setDetails({ ...details, username: e.target.value })} value={details.username} />
-                            </div>
-                            <div className="Signup_Input_Field">
-                                <input type="password" name="password" id="password" placeholder="Password" onChange={e => setDetails({ ...details, password: e.target.value })} value={details.password} />
-                            </div>
-                        </div>
+        </div>
+        {/* PageTitle*/}
+        <div className="Pagetitlecontainer">
+          <h3>{PageTitle}</h3>
+        </div>
 
 
-                        <div className={`${!LogResBoolean && "Signup_Input_Container_noshow"}  ${LogResBoolean && "Signup_Input_Container"}`}>
-                            <div className="Signup_Input_Field">
-                                <input type="text" name="name" id="name" placeholder="UserName" onChange={e => setDetails({ ...details, username: e.target.value })} value={details.username} />
-                            </div>
+        {/*Form */}
+        <form className="Login_form_container" onSubmit={submitHandler}>
+          <div className="errorcontainer">
 
-                            <div className="Signup_Input_Field">
-                                <input type="password" name="password" id="password" placeholder="Password" onChange={e => setDetails({ ...details, password: e.target.value })} value={details.password} />
-                            </div>
-                        </div>
-                    </div>
-                    <button className="Loginbutton" type='submit' value="LOGIN">{PageTitle}</button>
+          </div>
 
-                </form>
-                <div className="LogResToggleContainer">
-                    <button className="Login_Sign_Toggle" onClick={() => setLogResBoolean(!LogResBoolean)}>{Login_Register_Toggle}</button>
-                </div>
+          {/*Container*/}
+          <div className="Login_Signup_Input_Container">
+            <div className={`${LogResBoolean && "Login_Input_container_noshow"}  ${!LogResBoolean && "Login_Input_container "}`}>
+              <div className="Login_Input_Field">
+                <input placeholder="Login" onChange={e => setDetails({ ...details, username: e.target.value })} value={details.username} />
+              </div>
+              <div className="Signup_Input_Field">
+                <input type="password" name="password" id="password" placeholder="Password" onChange={e => setDetails({ ...details, password: e.target.value })} value={details.password} />
+              </div>
             </div>
 
 
+            <div className={`${!LogResBoolean && "Signup_Input_Container_noshow"}  ${LogResBoolean && "Signup_Input_Container"}`}>
+              <div className="Signup_Input_Field">
+                <input type="text" name="name" id="name" placeholder="UserName" onChange={e => setDetails({ ...details, username: e.target.value })} value={details.username} />
+              </div>
 
-            <style jsx>{`
+              <div className="Signup_Input_Field">
+                <input type="password" name="password" id="password" placeholder="Password" onChange={e => setDetails({ ...details, password: e.target.value })} value={details.password} />
+              </div>
+            </div>
+          </div>
+          <button className="Loginbutton" type='submit' value="LOGIN">{PageTitle}</button>
+
+        </form>
+        <div className="LogResToggleContainer">
+          <button className="Login_Sign_Toggle" onClick={() => setLogResBoolean(!LogResBoolean)}>{Login_Register_Toggle}</button>
+        </div>
+      </div>
+
+
+
+      <style jsx>{`
 
           /////////////////////////// - Login Page (height 100%)
 
@@ -346,7 +349,7 @@ export default function Login() {
           
 
         `}</style>
-        </div>
-    )
+    </div>
+  )
 }
 
